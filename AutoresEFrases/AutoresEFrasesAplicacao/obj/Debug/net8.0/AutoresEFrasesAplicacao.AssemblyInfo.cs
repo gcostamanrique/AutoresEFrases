26d08e294b4dc3cc5d3d707276cad5e7bd23405a
Guilherme Costa Manrique <gcostamanrique@gmail.com>
2025-01-08 10:39:08 -0300

    Update QuerySQLServer.txt

--- AutoresEFrases/AutoresEFrasesAplicacao/obj/Debug/net8.0/AutoresEFrasesAplicacao.AssemblyInfo.cs
+++ AutoresEFrases/AutoresEFrasesAplicacao/obj/Debug/net8.0/AutoresEFrasesAplicacao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoresEFrasesAplicacao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11c92bc465b9b1a4b176bef51622d207a7c40f45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d151a8d755ad7581b8ac0fb5021da968c2207c21")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoresEFrasesAplicacao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoresEFrasesAplicacao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
